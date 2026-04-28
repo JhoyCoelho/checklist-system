5de92f4642b58b3558a97fbb934ef7b72434ad5d
jhoy-coelho <jhoycoelho.oficial@gmail.com>
2026-04-28 15:12:53 -0300

    fix: externalize chromium for vercel

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ["@sparticuz/chromium"]
+  },
+  webpack: (config) => {
+    config.externals.push("@sparticuz/chromium");
+    return config;
+  }
+};
+
+module.exports = nextConfig;
